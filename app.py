096664d7799160e60c2ca973d6f8aee35c644ff0
SMRUTI RANJAN SAHOO <smrutiranjansahoo516@gmail.com>
2025-05-29 18:58:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import requests
 import hashlib
 import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # default to 5000 for local dev
-    app.run(host="0.0.0.0", port=port, debug=True)
-
-
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 def check_pwned(password):
     sha1 = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
