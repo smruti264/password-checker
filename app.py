bcc5cbee5b8af5f77bada73dfe7e2abc1cd1a365
SMRUTI RANJAN SAHOO <smrutiranjansahoo516@gmail.com>
2025-05-29 18:46:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 import hashlib
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 for local dev
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
 app = Flask(__name__)
 
